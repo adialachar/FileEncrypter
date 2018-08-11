899855fcc956dab445f61216672e15e5c9a4d9bf
Aditya Acharya <aacha002@ucr.edu>
2018-08-11 14:46:08 -0700

    Bootstrap layout started, basic RSA function written

--- FileEnc/models.py
+++ FileEnc/models.py
@@ -15,4 +15,30 @@ class Blowfish(models.Model):
 
 
 	
-	
+def RSAEnc(file_input_name,file_output_name,queue, p , q, e):
+
+	#with open(file_input_name, 'r')	
+
+	f = open(file_input_name, 'r')
+	g = open(file_ouput_name, 'r+')
+	enclist = []
+	for line in f:
+		for i in range(length(line)):
+
+			enclist[i] = cal( int(line[i]), int(e), int( int(p) * int(q))
+		
+		queue.push(enclist)
+		enclist.clear()
+
+
+
+	for line in queue
+
+		#write to a file 
+		g.write(line)
+
+
+	return g #idk what to return lol
+
+
+
